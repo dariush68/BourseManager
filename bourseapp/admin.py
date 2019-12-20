99bd27af2f6caf9362dd8d1709d4aa91d47c5870
dariush <dar@gmail.com>
2019-12-21 00:01:17 +0330

    s

--- bourseapp/admin.py
+++ bourseapp/admin.py
@@ -59,7 +59,61 @@ class NewsAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
         return redirect('bourseapp:news-list')
 
 
+@admin.register(models.Technical)
+class TechnicalAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    formfield_overrides = {
+    }
+
+    # override add function
+    def response_add(self, request, obj, post_url_continue=None):
+        return redirect('bourseapp:technical-list')
+
+    # override edit function
+    def response_change(self, request, obj):
+        return redirect('bourseapp:technical-list')
+
+    # override delete function
+    def response_delete(self, request, obj_display, obj_id):
+        return redirect('bourseapp:technical-list')
+
+
+@admin.register(models.Fundamental)
+class FundamentalAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    formfield_overrides = {
+    }
+
+    # override add function
+    def response_add(self, request, obj, post_url_continue=None):
+        return redirect('bourseapp:fundamental-list')
+
+    # override edit function
+    def response_change(self, request, obj):
+        return redirect('bourseapp:fundamental-list')
+
+    # override delete function
+    def response_delete(self, request, obj_display, obj_id):
+        return redirect('bourseapp:fundamental-list')
+
+
+@admin.register(models.Targets)
+class TargetslAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
+    formfield_overrides = {
+    }
+
+    # override add function
+    def response_add(self, request, obj, post_url_continue=None):
+        return redirect('bourseapp:targets-list')
+
+    # override edit function
+    def response_change(self, request, obj):
+        return redirect('bourseapp:targets-list')
+
+    # override delete function
+    def response_delete(self, request, obj_display, obj_id):
+        return redirect('bourseapp:targets-list')
+
+
 # admin.site.register(models.Category)
-admin.site.register(models.Fundamental)
-admin.site.register(models.Targets)
-admin.site.register(models.Technical)
+# admin.site.register(models.Fundamental)
+# admin.site.register(models.Targets)
+# admin.site.register(models.Technical)
--- bourseapp/views.py
+++ bourseapp/views.py
@@ -29,8 +29,13 @@ from jalali_date import datetime2jalali, date2jalali
 def index(request):
     # jalali_join = datetime2jalali(request.user.date_joined).strftime('%y/%m/%d _ %H:%M:%S')
 
+    news = models.News.objects.all()[0:10]
+    targets = models.Targets.objects.all()
+
     # if request.user.is_authenticated:
     return render(request, 'bourseapp/index.html', {
+        'news': news,
+        'targets': targets,
     })
 
     # HttpResponseRedirect(reverse('admin:login'))
@@ -115,4 +120,147 @@ def new_list(request):
         'newss': news,
         'search': search,
         'page_size': page_size
+    })
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def technical_list(request):
+    page = request.GET.get('page', 1)
+    page_size = request.GET.get('page-size', 10)
+    search = request.GET.get('search', '')
+
+    technical = models.Technical.objects.filter(Q(company__symbol__icontains=search)
+                                      | Q(createAt__icontains=search)
+                                      | Q(description__icontains=search)
+                                      )
+    paginator = Paginator(technical, page_size)
+    try:
+        technical = paginator.page(page)
+    except PageNotAnInteger:
+        technical = paginator.page(1)
+    except EmptyPage:
+        technical = paginator.page(paginator.num_pages)
+
+    return render(request, 'bourseapp/technical_list.html', {
+        'technical': technical,
+        'search': search,
+        'page_size': page_size
+    })
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def fundamental_list(request):
+    page = request.GET.get('page', 1)
+    page_size = request.GET.get('page-size', 10)
+    search = request.GET.get('search', '')
+
+    fundamentals = models.Fundamental.objects.filter(Q(company__symbol__icontains=search)
+                                      | Q(createAt__icontains=search)
+                                      | Q(description__icontains=search)
+                                      )
+    paginator = Paginator(fundamentals, page_size)
+    try:
+        fundamentals = paginator.page(page)
+    except PageNotAnInteger:
+        fundamentals = paginator.page(1)
+    except EmptyPage:
+        fundamentals = paginator.page(paginator.num_pages)
+
+    return render(request, 'bourseapp/fundamental_list.html', {
+        'fundamentals': fundamentals,
+        'search': search,
+        'page_size': page_size
+    })
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def targets_list(request):
+    page = request.GET.get('page', 1)
+    page_size = request.GET.get('page-size', 10)
+    search = request.GET.get('search', '')
+
+    targets = models.Targets.objects.filter(Q(company__symbol__icontains=search)
+                                      | Q(createAt__icontains=search)
+                                      | Q(description__icontains=search)
+                                      )
+    paginator = Paginator(targets, page_size)
+    try:
+        targets = paginator.page(page)
+    except PageNotAnInteger:
+        targets = paginator.page(1)
+    except EmptyPage:
+        targets = paginator.page(paginator.num_pages)
+
+    return render(request, 'bourseapp/targets_list.html', {
+        'targets': targets,
+        'search': search,
+        'page_size': page_size
+    })
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def category_detail(request, category_id):
+    page = request.GET.get('page', 1)
+    page_size = request.GET.get('page-size', 10)
+    search = request.GET.get('search', '')
+
+    companies = models.Company.objects.filter((Q(symbol__icontains=search)
+                                              | Q(fullName__icontains=search)
+                                              | Q(bourseType__icontains=search)
+                                              | Q(createAt__icontains=search)
+                                              | Q(description__icontains=search))
+                                              & Q(category=category_id)
+                                              )
+    category = models.Category.objects.get(id=category_id)
+    paginator = Paginator(companies, page_size)
+    try:
+        companies = paginator.page(page)
+    except PageNotAnInteger:
+        companies = paginator.page(1)
+    except EmptyPage:
+        companies = paginator.page(paginator.num_pages)
+
+    return render(request, 'bourseapp/category_detail.html', {
+        'category': category,
+        'companies': companies,
+        'search': search,
+        'page_size': page_size
+    })
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def company_detail(request, company_id):
+    company = get_object_or_404(models.Company, pk=company_id)
+    news = models.News.objects.filter(company=company.id)
+    technicals = models.Technical.objects.filter(company=company.id)
+    fundamental = models.Fundamental.objects.filter(company=company.id)
+    return render(request, 'bourseapp/compay_detail.html', {
+        'company': company,
+        'news': news,
+        'technicals': technicals,
+        'fundamentals': fundamental,
+    })
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def news_detail(request, news_id):
+    news = get_object_or_404(models.News, pk=news_id)
+    return render(request, 'bourseapp/news_detail.html', {
+        'news': news,
+    })
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def technical_detail(request, technical_id):
+    technical = get_object_or_404(models.Technical, pk=technical_id)
+    return render(request, 'bourseapp/technical_detail.html', {
+        'technical': technical,
+    })
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def fundamental_detail(request, fundamental_id):
+    fundamental = get_object_or_404(models.Fundamental, pk=fundamental_id)
+    return render(request, 'bourseapp/fundamental_detail.html', {
+        'fundamental': fundamental,
     })
